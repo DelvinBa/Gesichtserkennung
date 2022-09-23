65b65071dd6dd7d1eae3bd4bcd922671e4517224
Splashy205 <114254602+Splashy205@users.noreply.github.com>
2022-09-23 20:19:19 +0200

    Update gesichtserkennung.py

--- gesichtserkennung.py
+++ gesichtserkennung.py
@@ -1,16 +1,14 @@
 import cv2
 
-capture = cv2.VideoCapture(0)  # steuert die Kamera an
-# Orte wo Gesichter sich befinden
-cascade = cv2.CascadeClassifier(
-    "Users/menke/Desktop/opencv_data_haarcascades%20at%20master%20·%20opencv_opencv%20·%20GitHub.html")
+capture = cv2.VideoCapture(0) #steuert die Kamera an
+cascade = cv2.CascadeClassifier(r"C:\Users\jonas\Desktop\haarcascade_frontalface_default.xml") #Orte wo Gesichter sich befinden
 
 while True:
     _, im = capture.read()
-    # im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #in scharz/weiß Bilder umwandeln
-   # faces = cascade.detectMultiScale(image_gray)
-    # for x,y, width, height in faces:
-    #   cv2.rectangle(image, (x,y), (x + width, y + height), color=(0, 0, 255), thickness=5)
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #in scharz/weiß Bilder umwandeln
+    faces = cascade.detectMultiScale(im_gray)
+    for x,y, width, height in faces:
+        cv2.rectangle(im, (x,y), (x + width, y + height), color=(0, 0, 255), thickness=5)
     cv2.imshow("Kamera", im)
     if cv2.waitKey(1) == ord("q"):
         break
