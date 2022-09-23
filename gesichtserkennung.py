1ed2cf248d06eda461304ab95cf0e34bfbb8f26e
Delvin7 <delvin.bacho@wolkenmacher.gmbh>
2022-09-23 20:27:53 +0200

    tests

--- gesichtserkennung.py
+++ gesichtserkennung.py
@@ -1,16 +1,21 @@
 import cv2
 
-capture = cv2.VideoCapture(0) #steuert die Kamera an
-cascade = cv2.CascadeClassifier(r"C:\Users\jonas\Desktop\haarcascade_frontalface_default.xml") #Orte wo Gesichter sich befinden
+capture = cv2.VideoCapture(0)  # steuert die Kamera an
+# Orte wo Gesichter sich befinden
+cascade = cv2.CascadeClassifier(
+    r"C:\Users\jonas\Desktop\haarcascade_frontalface_default.xml")
 
 while True:
     _, im = capture.read()
-    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #in scharz/weiß Bilder umwandeln
+    # in scharz/weiß Bilder umwandeln
+    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(im_gray)
-    for x,y, width, height in faces:
-        cv2.rectangle(im, (x,y), (x + width, y + height), color=(0, 0, 255), thickness=5)
+    for x, y, width, height in faces:
+        cv2.rectangle(im, (x, y), (x + width, y + height),
+                      color=(0, 0, 255), thickness=5)
     cv2.imshow("Kamera", im)
     if cv2.waitKey(1) == ord("q"):
         break
 capture.realease()
 cv2.destroyAllWindows()
+print
