2338a3bbdaa36eadb51fd4296d7db3555c7c61ba
delvin b <bachodelvin@gmail.com>
2022-09-24 17:42:41 +0200

    Signale senden

--- faceregraspi.py
+++ faceregraspi.py
@@ -7,10 +7,19 @@
 # https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
 
 from symbol import for_stmt
+from time import sleep
+from turtle import delay
 import face_recognition
 import picamera
 import numpy as np
 import json
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("172.16.2.5", 1883, 60)
+client.publish("securedoor/face", 3)
+    
+print("signal geschickt")
 # Get a reference to the Raspberry Pi camera.
 # If this fails, make sure you have a camera connected to the RPi and that you
 # enabled your camera in raspi-config and rebooted first.
@@ -38,11 +47,16 @@ while True:
     #print("Capturing image.")
     # Grab a single frame of video from the RPi camera as a numpy array
     camera.capture(output, format="rgb")
+    
 
     # Find all the faces and face encodings in the current frame of video
     face_locations = face_recognition.face_locations(output)
     print("Found {} faces in image.".format(len(face_locations)))
     face_encodings = face_recognition.face_encodings(output, face_locations)
+    
+    if len(face_locations) == 0:
+        client.publish("securedoor/face", 3)
+        continue 
 
     # Loop over each face found in the frame to see if it's someone we know.
     for face_encoding in face_encodings:
@@ -52,5 +66,12 @@ while True:
 
         for i in range(len(match)): 
             if match[i]:
-                print("     " + faces[i]["name"] + " is authorized: " + str(faces[i]["authorized"])) 
+                print("     " + faces[i]["name"] + " is authorized: " + str(faces[i]["authorized"]))
+                if(faces[i]["authorized"] == True ) :
+                    client.publish("securedoor/face", 1)
+                else :
+                    client.publish("securedoor/face", 2)
+            else :
+                print(name)
+                client.publish("securedoor/face", 0)
 
